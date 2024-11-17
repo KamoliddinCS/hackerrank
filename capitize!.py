e6c7c6220b6b47bbc3cfedfddcb62053c9e60a14
Kamoliddin <ulbarsovk@gmail.com>
2024-11-18 00:02:55 +0900

    Solution for Capitalize! problem added.

---
+++ capitize!.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+from string import capwords
+
+# Complete the solve function below.
+def solve(s):
+    return re.sub(r'\b(\d*[A-Za-z])', lambda mo: mo.group(0)[0].upper() + mo.group(0)[1:], s)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
