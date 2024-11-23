0f4be8a7caa759cd2325ac7f41e643d6c0686c37
Kamoliddin <ulbarsovk@gmail.com>
2024-11-23 22:12:01 +0900

    Some spaces added to capitalize.py.

--- capitize!.py
+++ capitize!.py
@@ -5,9 +5,11 @@ import re
 import sys
 from string import capwords
 
+
 # Complete the solve function below.
 def solve(s):
     return re.sub(r'\b(\d*[A-Za-z])', lambda mo: mo.group(0)[0].upper() + mo.group(0)[1:], s)
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
